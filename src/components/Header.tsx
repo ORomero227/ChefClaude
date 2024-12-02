d53ed46499d58f0fdefadf9a75ed247ccd074888
Oscar Romero <oscalito992@gmail.com>
2024-12-02 16:55:12 -0400

    Header component added

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <header className="py-3 flex items-center gap-2 justify-center bg-white shadow-md">
+      <img src="/src/assets/chef-icon.svg" className="size-11" />
+      <h1 className="font-inter text-2xl">Chef Claude</h1>
+    </header>
+  );
+}
+
+export default Header;
