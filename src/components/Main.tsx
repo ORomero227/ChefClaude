4a93ca7fc7c76bb005e2d60d0ecbcf15359faea4
Oscar Romero <oscalito992@gmail.com>
2024-12-05 12:52:43 -0400

    Main component added

---
+++ src/components/Main.tsx
@@ -0,0 +1,12 @@
+function Main() {
+  return (
+    <main className="flex justify-center h-screen p-4 md:p-0">
+      <div className="flex flex-col pt-16">
+        <section>Ingredients Section Here</section>
+        <section>Claude Recipe Section Here</section>
+      </div>
+    </main>
+  );
+}
+
+export default Main;
