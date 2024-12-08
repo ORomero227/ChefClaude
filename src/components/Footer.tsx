9dc387b61c5657579f358664d968076088863877
Oscar Romero <oscalito992@gmail.com>
2024-12-08 18:37:43 -0400

    Footer added

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <footer className="m-4 mt-16 max-w-xl mx-auto container mb-10 text-center">
+      <span className="text-base font-inter">
+        🧑🏾‍💻 Developed by Oscar Romero Barbosa
+      </span>
+    </footer>
+  );
+}
+
+export default Footer;
