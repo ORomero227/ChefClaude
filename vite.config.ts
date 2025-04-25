b4a9a487c324a873efa25c5d2f9cc12ff13c2931
Oscar Romero <oscalito992@gmail.com>
2025-04-25 08:59:29 -0400

    Alias for all the folders added vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,15 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+      "@assets": path.resolve(__dirname, "src/assets"),
+      "@components": path.resolve(__dirname, "src/components"),
+      "@hooks": path.resolve(__dirname, "src/hooks"),
+      "@layouts": path.resolve(__dirname, "src/layouts"),
+      "@pages": path.resolve(__dirname, "src/pages"),
+      "@routes": path.resolve(__dirname, "src/routes"),
+      "@stores": path.resolve(__dirname, "src/stores"),
+      "@styles": path.resolve(__dirname, "src/styles"),
+      "@utils": path.resolve(__dirname, "src/utils"),
     },
   },
 });
